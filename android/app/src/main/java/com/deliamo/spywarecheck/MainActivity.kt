7082450a00a75ad98fee904d0bf1893e8d0ae5a1
talviphoenix <talviphoenix@gmail.com>
2025-12-28 18:04:46 +0100

    feat: add backwards logic to navigation

--- android/app/src/main/java/com/deliamo/spywarecheck/MainActivity.kt
+++ android/app/src/main/java/com/deliamo/spywarecheck/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -47,32 +46,40 @@ fun SpywareCheckApp() {
 
         composable(Routes.QUICK_CHECK) {
             QuickCheckScreen(
-                onDone = { navController.navigate(Routes.RESULT) }
+                onBack = { navController.popBackStack() },
+                onDone = { navController.navigate(Routes.RESULT) },
+                onQuickExit = {},
             )
         }
 
         composable(Routes.RESULT) {
             ResultScreen(
-
+                onBack = { navController.popBackStack() },
+                onQuickExit = {},
             )
         }
 
         composable(Routes.SAFETY_GATE) {
             SafetyGateScreen(
+                onBack = { navController.popBackStack() },
                 onContinue = { navController.navigate(Routes.SCAN) },
                 onCancel = { navController.popBackStack() },
+                onQuickExit = {},
             )
         }
 
         composable(Routes.SCAN) {
             ScanScreen(
+                onBack = { navController.popBackStack() },
                 onOpenFinding = { navController.navigate(Routes.FINDING) },
+                onQuickExit = {},
             )
         }
 
         composable(Routes.FINDING) {
             FindingDetailScreen(
                 onBack = { navController.popBackStack() },
+                onQuickExit = { }
             )
         }
 
