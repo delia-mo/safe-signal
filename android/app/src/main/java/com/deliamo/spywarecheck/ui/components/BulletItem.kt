d87fd970c12949bdb8135f64c02f8da0e08472ea
talviphoenix <talviphoenix@gmail.com>
2026-01-07 14:17:29 +0100

    chore: create bullet item component

---
+++ android/app/src/main/java/com/deliamo/spywarecheck/ui/components/BulletItem.kt
@@ -0,0 +1,19 @@
+package com.deliamo.spywarecheck.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BulletItem(text: String) {
+    Row(modifier = Modifier.padding(vertical = 4.dp)) {
+        Text("•  ")
+        Text(text, style = MaterialTheme.typography.bodyMedium)
+    }
+}
