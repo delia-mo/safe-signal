86e382cabf1dcfb325b38039041206259e637192
Delia <talviphoenix@gmail.com>
2026-01-26 22:18:22 +0100

    chore: adjust colors

--- android/app/src/main/java/com/deliamo/spywarecheck/ui/theme/Color.kt
+++ android/app/src/main/java/com/deliamo/spywarecheck/ui/theme/Color.kt
@@ -7,7 +7,7 @@ val PrimaryLight = Color(0xFF0F766E)      // teal-700 (primary - light theme)
 val SecondaryLight = Color(0xFF0891B2)  // cyan-600 (secondary)
 val TertiaryLight = Color(0xFF0EA5E9)        // sky-500 (tertiary)
 
-val PrimaryDark = Color(0xFF5EEAD4)      // teal-300 (primary - dark theme)
+val PrimaryDark = Color(0xFF2DD4BF)      // teal-300 (primary - dark theme)
 val SecondaryDark = Color(0xFF67E8F9)  // cyan-300 (secondary)
 val TertiaryDark = Color(0xFF7DD3FC)        // sky-300 (tertiary)
 
@@ -16,11 +16,11 @@ val BgLight = Color(0xFFF8FAFC)       // slate-50
 val SurfaceLight = Color(0xFFFFFFFF)
 val SurfaceTint = Color(0xffDDEFF2)
 val OnDark = Color(0xFF0F172A)        // slate-900
-val OutlineLight = Color(0xFF8EA4A4)  // slate-200
-val SurfaceVariantLight = Color(0xFFC4D5DA)
+val OutlineLight = Color(0xFFCBD5E1)  // slate-200
+val SurfaceVariantLight = Color(0xFFCFE4E2)
 val OutlineVariantLight = Color(0xFFAAB9C0)
-val OnSurfaceVariantLight = Color(0xFF334155) // gut lesbar
-val SecondaryContainerLight = Color(0xFFC3E2E7)
+val OnSurfaceVariantLight = Color(0xFF334155)
+val SecondaryContainerLight = Color(0xFFB7D6D4)
 val OnSecondaryContainerLight = OnDark               // slate-900
 
 
@@ -30,7 +30,7 @@ val OnLight = Color(0xFFE2E8F0)       // slate-200
 val SurfaceVariantDark = Color(0xFF222937)    // leicht heller als SurfaceDark
 val OutlineVariantDark = Color(0xFF3F4653)
 val OnSurfaceVariantDark = OnLight
-val SecondaryContainerDark = Color(0xFF155E75)       // cyan-800-ish (oder dunkles teal/cyan)
+val SecondaryContainerDark = Color(0xFF156775)       // cyan-800-ish (oder dunkles teal/cyan)
 val OnSecondaryContainerDark = OnLight               // slate-200
 
 
